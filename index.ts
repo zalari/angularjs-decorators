d3443199e8ecdf7a5c7737d61f531cada6a3eeec
Christian Ulbrich <christian.ulbrich@zalari.de>
2016-07-28 00:33:51 +0200

    initial stuff

---
+++ index.ts
@@ -0,0 +1,5 @@
+const LegacyComponent = function(options: ng.IComponentOptions): Function {
+    return (controller: Function) => {
+        return angular.extend(options, {controller});
+    };
+};
