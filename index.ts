21f191110bb79a41e70e494c6247f5a99ebad30a
Christian Ulbrich <christian.ulbrich@zalari.de>
2016-09-18 01:04:08 +0200

    another jab...

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
+export declare namespace ngxt {
+
+  interface IComponentOptions extends ng.IComponentOptions {
+    /**
+     * this is a convenient field, to actually name "directive" components and allow for easier
+     * hookup with angular.module().component registration
+     */
+    selector?: string;
+  }
+
+}
+
 export const LegacyComponent = function (options: ngxt.IComponentOptions):Function {
   return (controller: Function) => {
     return angular.extend(options, {controller});
