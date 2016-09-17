ff70ed25fa9882cd12f31c34e6e7fc0d243b724d
Christian Ulbrich <christian.ulbrich@zalari.de>
2016-09-18 00:35:18 +0200

    fix declaration

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,11 @@
-namespace ngxt {
+declare namespace ngxt {
 
   interface IComponentOptions extends ng.IComponentOptions {
-    selector: string;
+    /**
+     * this is a convenient field, to actually name "directive" components and allow for easier
+     * hookup with angular.module().component registration
+     */
+    selector?: string;
   }
 
 }
